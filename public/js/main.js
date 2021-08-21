65c15792175b0711d9bbcb6754ed1ed04925a1d8
BLooDBRothER <computertechnology446@gmail.com>
2021-08-21 17:33:01 +0530

    Add signin and logout functionality

---
+++ public/js/main.js
@@ -0,0 +1,36 @@
+import { firebaseConfig, userSignOut } from "./modules/firebase.js";
+
+// firebase initialization
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+
+const auth = firebase.auth();
+const database = firebase.database();
+let user;
+
+// selector
+const userProfilePic = document.querySelector(".header__img");
+const userName = document.querySelector(".header__title--username");
+
+//update profile and name
+function updateUserDetails(){
+    userProfilePic.src = user.photoURL;
+    userName.innerText = user.displayName;
+}
+
+// sign In status change
+auth.onAuthStateChanged((check_user) => {
+  if (check_user) {
+    user = check_user;
+    console.log(user);
+    updateUserDetails();
+  } else {
+      window.location = "./sign_in.html"
+    console.log("out");
+  }
+});
+
+// listener
+userProfilePic.addEventListener("click", ()=>{
+    userSignOut(auth);
+});
