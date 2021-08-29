a90e072d6c0cd3df6eb1e9aea187da43f7ada929
BLooDBRothER <computertechnology446@gmail.com>
2021-08-29 11:14:43 +0530

    pause play implement

--- public/js/file_upload.js
+++ public/js/file_upload.js
@@ -1,4 +1,11 @@
-import { pushKey, storageDelete, storageDownloadURL, storageList, storageRef, storageUpload } from "./modules/firebase.js";
+import {
+  pushKey,
+  storageDelete,
+  storageDownloadURL,
+  storageList,
+  storageRef,
+  storageUpload,
+} from "./modules/firebase.js";
 
 const toggleUploadBtn = document.querySelector(".main__img--file");
 const uploadCnt = document.querySelector(".upload");
@@ -10,7 +17,6 @@ const fileUploadClick = document.querySelectorAll(".upload__click--each");
 const sendBtn = document.querySelector(".main__img--send");
 const inputChat = document.querySelector(".main__input--chat");
 const chatContainer = document.querySelector(".main__chat-container");
-
 
 let fileToUpload = [];
 
@@ -95,14 +101,14 @@ fileUploadClick.forEach((upload) => {
 
 toggleUploadBtn.addEventListener("click", toggleUploadWindow);
 
-function toggleUploadWindow(){
+function toggleUploadWindow() {
   chat.classList.toggle("none");
   uploadCnt.classList.toggle("none");
   inputChat.value = "";
   inputChat.disabled = !inputChat.disabled;
 }
 
-function createImagePreview(key, src, size){
+function createImagePreview(key, src, size, upTask) {
   chatContainer.innerHTML += `
                 <div class="main__message-container main__message-container--right" data-type="image" data-id="${key}">
                   <div class="main__message--image-cnt">
@@ -120,14 +126,23 @@ function createImagePreview(key, src, size){
                   </div>
                   <span class="main__time-stamp main__time-stamp--left">23/20/23, 9:30pm</span>
                 </div>`;
+  let pausePlay = document.querySelector(
+    `.main__message-container[data-id="${key}"] .main__message--controls`
+  );
+  pausePlay.addEventListener("click", (e) => {
+    console.log(e.target);
+    e.target.src.includes("play")
+      ? ((e.target.src = "./assets/icons/home/pause.svg"), upTask.pause())
+      : ((e.target.src = "./assets/icons/home/play.svg"), upTask.resume());
+  });
 }
 
-function createFilePrevieew(key, name, size){
+function createFilePrevieew(key, name, size, upTask) {
   chatContainer.innerHTML += `
                 <div class="main__message-container main__message-container--right" data-type="file" data-id="${key}">
                   <div class="main__message--file-cnt">
                     <div class="main__message--file-download">
-                      <img class="main__message--file-controls" src="./assets/icons/home/play.svg" class="main__message--controls"  alt="">  
+                      <img class="main__message--file-controls" src="./assets/icons/home/play.svg" alt="">  
                       <div class="main__message--progress">
                         <svg>
                           <circle cx="40" cy="40" r="40"></circle>
@@ -143,10 +158,16 @@ function createFilePrevieew(key, name, size){
                   </div>
                   <span class="main__time-stamp main__time-stamp--left">23/20/23, 9:30pm</span>
               </div>`;
+  const pausePlay = document.querySelector(`.main__message-container[data-id="${key}"] .main__message--file-controls`);
+  pausePlay.addEventListener("click", (e) => {
+    console.log(e.target);
+    e.target.src.includes("play")
+      ? ((e.target.src = "./assets/icons/home/pause.svg"), upTask.pause())
+      : ((e.target.src = "./assets/icons/home/play.svg"), upTask.resume());
+  });
 }
 
-function updateImagePreview(cnt, link){
-
+function updateImagePreview(cnt, link) {
   const imgLink = cnt.querySelector(".main__message--image");
   const aLink = cnt.querySelector(".main__message--link");
   const size = cnt.querySelector(".main__message--downloaded");
@@ -155,13 +176,12 @@ function updateImagePreview(cnt, link){
   imgLink.src = link;
   aLink.href = link;
 
-  const fromRemove = cnt.querySelector(".main__message--image-cnt")
+  const fromRemove = cnt.querySelector(".main__message--image-cnt");
   const toRemove = fromRemove.querySelector(".main__message--data");
   fromRemove.removeChild(toRemove);
 }
 
-function updateFilePreview(cnt, link){
-
+function updateFilePreview(cnt, link) {
   const imgLink = cnt.querySelector(".main__message--download-ic");
   const aLink = cnt.querySelector(".main__message--link");
   const name = cnt.querySelector(".main__message--file-name");
@@ -180,77 +200,99 @@ function updateFilePreview(cnt, link){
   fromRemove = cnt.querySelector(".main__message--file-detail");
   toRemove = cnt.querySelector(".main__message--cancel");
   fromRemove.removeChild(toRemove);
-
 }
 
 const storage = firebase.storage();
 const database = firebase.database();
 sendBtn.addEventListener("click", async (e) => {
-  if(uploadCnt.classList.contains("none")) return;
+  if (uploadCnt.classList.contains("none")) return;
   toggleUploadWindow();
-  fileToUpload.forEach(file => {
-    const size = (file.size/(1024*1024)).toFixed(2);
+  fileToUpload.forEach((file) => {
+    const size = (file.size / (1024 * 1024)).toFixed(2);
     const ref = storageRef(storage, `chat/chat1`, `${file.name}`);
-    const key = pushKey(database, `chat/chat1`,`${inputChat.dataset.chatHash}`);
+    const key = pushKey(
+      database,
+      `chat/chat1`,
+      `${inputChat.dataset.chatHash}`
+    );
     console.log(key);
-    
-    (file.type.match(/image\//i)) ? createImagePreview(key, URL.createObjectURL(file), size) : createFilePrevieew(key, file.name, size);
 
     const metadata = {
       name: file.name,
-      size 
+      size,
     };
     let val = storageUpload(ref, file, metadata);
+    file.type.match(/image\//i)
+      ? createImagePreview(key, URL.createObjectURL(file), size, val)
+      : createFilePrevieew(key, file.name, size, val);
     console.log(ref);
     task(val, key);
   });
 });
 
-function task(uploadTask, key){
-  uploadTask.on('state_changed', 
-  (snapshot) => {
-    var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-    let byteTransfer = (snapshot.bytesTransferred/(1024 * 1024)).toFixed(2);
-    let byteTotal = (snapshot.totalBytes/(1024 * 1024)).toFixed(2);
-    
-    //update meta-data
-    const cnt = document.querySelector(`.main__message-container[data-id="${key}"]`);
-    const progressBar = cnt.querySelector(`.main__message--progress svg circle`);
-    const size = cnt.querySelector(".main__message--downloaded");
-    if(cnt.dataset.type === "file"){
-      progressBar.style.animation = progress === 0 || progress === 100 ? "uploading-file 2s linear infinite" : "unset";
-    }
-    else{
-      progressBar.style.animation = progress === 0 || progress === 100 ? "uploading-image 2s linear infinite" : "unset";
-    }
-    progressBar.style.strokeDashoffset = cnt.dataset.type === "image" ? (380 - (380 * progress) / 100) : (260 - (260 * progress) / 100) ;
-    size.innerText = `${byteTransfer} / ${byteTotal}MB`; 
-    console.log('Upload is ' + progress + '% done');
-    switch (snapshot.state) {
-      case firebase.storage.TaskState.PAUSED: // or 'paused'
-        console.log('Upload is paused');
-        break;
-      case firebase.storage.TaskState.RUNNING: // or 'running'
-        console.log('Upload is running');
-        break;
-    }
-  }, 
-  (error) => {
-    // Handle unsuccessful uploads
-  }, 
-  async () => {
-    const downloadURL = await storageDownloadURL(uploadTask.snapshot.ref);
-    const cnt = document.querySelector(`.main__message-container[data-id="${key}"]`);
-    cnt.dataset.type === "image" ? updateImagePreview(cnt, downloadURL) : updateFilePreview(cnt, downloadURL);
+function task(uploadTask, key) {
+  uploadTask.on(
+    "state_changed",
+    (snapshot) => {
+      var progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+      let byteTransfer = (snapshot.bytesTransferred / (1024 * 1024)).toFixed(2);
+      let byteTotal = (snapshot.totalBytes / (1024 * 1024)).toFixed(2);
 
-    // const img = document.querySelector(`.local-cnt[data-id="${key}"] .main__message--image`);
-    // const imgLink = document.querySelector(`.local-cnt[data-id="${key}"] .main__message--link`);
-    // img.src = downloadURL;
-    // imgLink.href = downloadURL;
-    // const cnt = document.querySelector(`.local-data-cnt[data-id="${key}"]`);
-    // const toRemove = document.querySelector(`.local-data-cnt[data-id="${key}"] .local-remove`);
-    // console.log(cnt, toRemove);
-    // cnt.removeChild(toRemove); 
-  }
-);
+      //update meta-data
+      const cnt = document.querySelector(
+        `.main__message-container[data-id="${key}"]`
+      );
+      const progressBar = cnt.querySelector(
+        `.main__message--progress svg circle`
+      );
+      const size = cnt.querySelector(".main__message--downloaded");
+      if (cnt.dataset.type === "file") {
+        progressBar.style.animation =
+          progress === 0 || progress === 100
+            ? "uploading-file 2s linear infinite"
+            : "unset";
+      } else {
+        progressBar.style.animation =
+          progress === 0 || progress === 100
+            ? "uploading-image 2s linear infinite"
+            : "unset";
+      }
+      progressBar.style.strokeDashoffset =
+        cnt.dataset.type === "image"
+          ? 380 - (380 * progress) / 100
+          : 260 - (260 * progress) / 100;
+      size.innerText = `${byteTransfer} / ${byteTotal}MB`;
+      console.log("Upload is " + progress + "% done");
+      switch (snapshot.state) {
+        case firebase.storage.TaskState.PAUSED: // or 'paused'
+          console.log("Upload is paused");
+          break;
+        case firebase.storage.TaskState.RUNNING: // or 'running'
+          console.log("Upload is running");
+          break;
+      }
+    },
+    (error) => {
+      // Handle unsuccessful uploads
+      console.log(error);
+    },
+    async () => {
+      const downloadURL = await storageDownloadURL(uploadTask.snapshot.ref);
+      const cnt = document.querySelector(
+        `.main__message-container[data-id="${key}"]`
+      );
+      cnt.dataset.type === "image"
+        ? updateImagePreview(cnt, downloadURL)
+        : updateFilePreview(cnt, downloadURL);
+
+      // const img = document.querySelector(`.local-cnt[data-id="${key}"] .main__message--image`);
+      // const imgLink = document.querySelector(`.local-cnt[data-id="${key}"] .main__message--link`);
+      // img.src = downloadURL;
+      // imgLink.href = downloadURL;
+      // const cnt = document.querySelector(`.local-data-cnt[data-id="${key}"]`);
+      // const toRemove = document.querySelector(`.local-data-cnt[data-id="${key}"] .local-remove`);
+      // console.log(cnt, toRemove);
+      // cnt.removeChild(toRemove);
+    }
+  );
 }
