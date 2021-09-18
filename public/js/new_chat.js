e840ccc6d093032e80eb3ec5a43477c75778f028
BLooDBRothER <computertechnology446@gmail.com>
2021-09-18 19:00:26 +0530

    Add count functionality

---
+++ public/js/new_chat.js
@@ -0,0 +1,55 @@
+import { readDB } from "./modules/firebase.js";
+
+const auth = firebase.auth()
+const database = firebase.database();
+
+let user, friendsList;
+async function updateFriendsList() {
+    let friendsData = await readDB(database, `friends/${user.uid}`);
+    friendsList = friendsData.val().friends;
+}
+
+auth.onAuthStateChanged(async (currentUser) => {
+  if (currentUser) {
+    user = currentUser;
+    await updateFriendsList();
+    getEachChat();
+  }
+});
+
+function returnMessageCount(mid, message){
+    if(!message) return;
+    let bool = false, count=0;
+    Object.keys(message).forEach(msg => {
+        console.log(msg, mid)
+        if(bool) count++;
+        if(msg === mid) bool = true;
+    });
+    return count;
+}
+
+function updateMessageCount(hash, no){
+    const friendContainer = document.querySelector(`.chat__friend-card[data-hash="${hash}"]`);
+    let msgCountCnt = friendContainer.querySelector(".chat__message-count");
+    msgCountCnt.classList.remove("none");
+    msgCountCnt.innerText = no;
+}
+
+async function getEachChat(){
+    console.log(friendsList);
+    let notSeenNumber;
+    for(let fid in friendsList){
+        notSeenNumber = 0;
+        let hash = friendsList[fid];
+        let chat = await readDB(database, `chat/${hash}/lastSeenMessage/${user.uid}`);
+        console.log(chat.val());
+
+        const ref = (await firebase.database().ref(`chat/${hash}/messages`).orderByChild('sender')).equalTo(fid);
+        const messageIDS = (await ref.get()).val();
+        notSeenNumber = returnMessageCount(chat.val(), messageIDS);
+        console.log(notSeenNumber);
+        if(notSeenNumber){
+            updateMessageCount(hash, notSeenNumber)
+        }
+    }
+}
